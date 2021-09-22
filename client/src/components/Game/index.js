fc825838b993b733f99da60f7533b0f33994554e
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-09-21 20:42:42 -0400

    plz no errors

---
+++ client/src/components/Game/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react"
+import './style.css'
+
+const Game = () => {
+
+  const head = <div id="head"></div>
+
+
+  {
+    console.log("It worked")
+  }
+
+  console.log(React)
+
+
+  return (
+    <div id="eisle">
+      <div id="canvas">
+        {head}
+      </div>
+    </div>
+  )
+
+}
+
+export default Game;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Game from "../components/Game"
+
+// console.log(React)
+// console.log(Game)
+
+const Home = () => {
+    return (
+        <Game />
+    );
+};
+
+export default Home;
