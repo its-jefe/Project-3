e29fb058fb6bd60e921390e8ba59e47f5b85f618
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-09-24 14:09:00 -0400

    Deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/p3-snake', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  // useCreateIndex: true, DO WE NEED THIS??
   useFindAndModify: false
 });
 
