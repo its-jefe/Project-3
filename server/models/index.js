228fd440dc38a233c3df72a8f41bdcfea51dff59
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-09-24 20:59:31 -0400

    resolved?

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,3 @@
 const User = require("./User");
 
-<<<<<<< HEAD
-module.exports = { User, Product, Category, Order };
-=======
 module.exports = { User };
->>>>>>> e0294a7c1775000fed994b436b7cc934f0d3860c
